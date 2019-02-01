6a68997d9caac233092e589a853129d0267ec516
Tinker <GTinker@163.com>
2019-02-01 18:36:49 +0800

    udpate note

---
+++ src/design/pattern/chainofresponsibility/Handler.java
@@ -0,0 +1,5 @@
+package design.pattern.chainofresponsibility;
+
+public interface Handler {
+
+}
---
+++ src/design/pattern/chainofresponsibility/client.java
@@ -0,0 +1,9 @@
+package design.pattern.chainofresponsibility;
+
+public class client {
+
+    public static void main(String[] args) {
+
+    }
+
+}
